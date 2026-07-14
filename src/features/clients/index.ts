03ce2d6e911e7308dd3b275a7eb702d67eadb978
Adrian Toledo <toledosandro59@gmail.com>
2026-07-13 19:11:41 -0500

    feat(clients): add ClientListPage and barrel exports

---
+++ src/features/clients/index.ts
@@ -0,0 +1,6 @@
+export type { Client, ClientListFilters, PaginatedResponse } from './types';
+export { ClientListPage } from './pages/ClientListPage';
+export { ClientTable } from './components/ClientTable';
+export { ClientFilters } from './components/ClientFilters';
+export { useClients } from './hooks/use-clients';
+export { getClients } from './api/get-clients';
